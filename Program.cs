30e4e9edf04d4d65bd186ce08b3163fc4e0c7a08
ban0387035449 <72543287+ban0387035449@users.noreply.github.com>
2022-05-23 15:58:03 +0700

    Update Program.cs
    ok

--- Program.cs
+++ Program.cs
@@ -1,11 +1,16 @@
+using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using tintucbongda.Data;
+using tintucbongda.Models;
 
 namespace tintucbongda
 {
@@ -13,14 +18,26 @@ namespace tintucbongda
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            var host = CreateWebHostBuilder(args).Build();
+            using (var scope = host.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+                try
+                {
+                    var context = services.GetRequiredService<tintucbongdaContext>();
+                    context.Database.Migrate();
+                    data.Initialize(services);
+                }
+                catch (Exception exp)
+                {
+                    var logger = services.GetRequiredService<ILogger<Program>>();
+                    logger.LogError(exp, "Loi nap du lieu");
+                }
+            }
+            host.Run();
         }
 
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder.UseStartup<Startup>();
-                });
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args) => WebHost.CreateDefaultBuilder(args).UseStartup<Startup>();
     }
 }
+
